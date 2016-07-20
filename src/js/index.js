d72fe119361d04cc0bc11203a6b6eb1ef75f8159
Theres-Sophie Scheucher <theressophie@gmail.com>
2016-07-20 17:47:21 +0200

    WIP

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,18 @@
-const renderHello = require('./renderHello')
+(function() {
+    var FontFaceObserver = require('fontfaceobserver');
 
-module.exports = global.qck = {
-    renderHello,
-}
+        var fontASubset = new FontFaceObserver('OpenSansSubset');
+
+        Promise.all([fontASubset.load()]).then(function () {
+            document.documentElement.className += " fonts-stage-1";
+
+            var fontA = new FontFaceObserver('OpenSans');
+            var fontB = new FontFaceObserver('OpenSansBold');
+            var fontC = new FontFaceObserver('OpenSansItalic');
+
+            Promise.all([fontA.load(), fontB.load(), fontC.load()]).then(function () {
+                document.documentElement.className += " fonts-stage-2";
+
+            });
+        });
+    })();
